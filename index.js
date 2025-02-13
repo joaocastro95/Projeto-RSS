ce0beadde185b6225c5bd334437bc500cbbcaa62
Joao Victor Oliveira de Castro <joaov.castro95@gmail.com>
2025-02-13 14:34:25 -0300

    primeiro-commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import RSSParser from "rss-parser";
+
+const feedUrl = "https://sabe.io/rss.xml";
+
+const parse = async url => {
+    const feed = await new RSSParser().parseURL(url);
+
+    console.log(feed.title);
+
+    feed.items.forEach(item => {
+        console.log(`${item.title} - ${item.link}\n${item.contentSnippet}\n\n`);
+    });
+};
+
+console.log("Parsing " + feedUrl);
+
+parse(feedUrl);
